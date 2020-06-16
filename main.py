cbbcf4291fb3f73fcbcc9f015577942979a9201b
Alex <40242723+alxalx14@users.noreply.github.com>
2020-06-17 01:50:36 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,110 @@
+import threading
+import socket
+from time import sleep
+from datetime import datetime
+from pythonping import ping
+from sys import stdout, argv
+import json
+
+knownServices = {
+    21: "ftp",
+    22: "ssh",
+    53: "dns",
+    80: "http",
+    443: "https",
+    3306: "mysql",
+    3389: "rdp",
+    11211: "memcached"
+}
+
+class scan(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.targetIP = argv[1]
+        #print("\x1b[95m[\x1b[92mSCANNER\x1b[95m]\x1b[92mLoaded\x1b[97m:\x1b[95m%d \x1b[92mIPs\x1b[97m" % len(self.targetIP))
+        self.newDict = {}
+    
+
+    def recognizeServices(self, ip, port):
+        if(port in knownServices):
+            self.newDict[ip]["ports"][port] = knownServices[port]
+            print("\x1b[95m[\x1b[92mSCANNER\x1b[95m]\x1b[92mFound Port\x1b[97m: \x1b[95m%d \x1b[92mService\x1b[97m: \x1b[95m%s\x1b[97m" %  (port, knownServices[port]))
+        else:
+            print("\x1b[95m[\x1b[92mSCANNER\x1b[95m]\x1b[92mFound Port\x1b[97m: \x1b[95m%d \x1b[92mService\x1b[97m: \x1b[95mUnknown\x1b[97m" %  port)
+            self.newDict[ip]["ports"][port] = "unknown"
+    
+
+    def scan(self, ip, rangeToScan, tts):
+        sRange = int(rangeToScan.split('-')[0])
+        eRange = int(rangeToScan.split('-')[1])
+        for ports in range(sRange, eRange):
+            scanS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            scanS.settimeout(tts)
+
+            scanC = scanS.connect_ex((ip, ports))
+            if scanC == 0:
+                self.recognizeServices(ip, ports)
+                scanS.close()
+            else:
+                scanS.close()
+
+        
+
+    def run(self, ip):
+        activeThreads = []
+        self.totalSTime = datetime.now()
+        delayScan = ping(ip, size=32, count=4)
+
+        if(float(delayScan.rtt_avg_ms) >= 1000):
+            ourDelay = 0.08
+        else:
+            ourDelay = delayScan.rtt_avg_ms / 1000 + 0.01
+            
+
+        self.newDict[ip] = {}
+        self.newDict[ip]["ports"] = {}
+        port_a = 0
+        port_b = 1285
+        for x in range(0, 51):
+            port_range = "%d-%d" % (
+                port_a,
+                port_b
+            )
+            thread = threading.Thread(target=self.scan, args=(ip, port_range, ourDelay))
+            thread.start()
+            activeThreads.append(thread)
+            port_a += 1285
+            port_b += 1285
+
+        print("\x1b[95m[\x1b[92mSCANNER\x1b[95m]\x1b[92mAssigned \x1b[95m%d \x1b[92mThreads to\x1b[97m: \x1b[95m%s\x1b[97m" % (len(activeThreads), ip))
+        
+        for threads in activeThreads:
+            threads.join()
+            activeThreads.remove(threads)
+
+        if(22 in self.newDict[ip]['ports']):
+            self.newDict[ip]["type"] = "Possible server"
+        else:
+            self.newDict[ip]["type"] = "Possible home router"
+
+        print("\x1b[95m[\x1b[92mSCANNER\x1b[95m]\x1b[92mIP\x1b[97m: \x1b[95m%s \x1b[92mdone.\x1b[97m" % ip)
+        self.saveStats(self.newDict)
+
+
+    def saveStats(self, dictionary):
+        with open('data.json', 'w+')as  f:
+            json.dump(dictionary, f, indent=4)
+        print("\x1b[95m[\x1b[92mSCANNER\x1b[95m]\x1b[92mSaved infos to data.json\x1b[97m")
+
+
+    def main(self):
+        print("\x1b[95m[\x1b[92mSCANNER\x1b[95m]\x1b[92mStarting threads for scanning.\x1b[97m")
+        self.run(self.targetIP)
+        
+        sleep(0.2)
+        t2 = datetime.now()
+        total = t2 - self.totalSTime
+        print("\x1b[95m[\x1b[92mSCANNER\x1b[95m]\x1b[92mTotal Scanning Time\x1b[97m: \x1b[95m%s\x1b[97m" % total)
+        return True
+
+scan().main()
